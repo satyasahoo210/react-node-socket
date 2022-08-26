e6934333b4a92e94f00dd3ef87b978fbe5a4648b
Satya <satyasahoo210@gmail.com>
2022-08-26 06:50:43 +0530

    second react app created

---
+++ project2/src/config.js
@@ -0,0 +1,12 @@
+const UPDATE = "UPDATE";
+const SAVE = "SAVE";
+const BROADCAST = "BROADCAST";
+
+
+export const ENDPOINT = "http://127.0.0.1:4001";
+
+export const events = {
+    UPDATE,
+    SAVE,
+    BROADCAST
+};
